6df690b1085dc35e213607b20fab4908d352388d
Damar2121 <damar.vivoy51@gmail.com>
2024-04-01 16:55:18 +0700

    menambah array, menambah fungsi for ke int main, dan hasil akhir

--- StructC_4/StructC_4.cpp
+++ StructC_4/StructC_4.cpp
@@ -13,3 +13,33 @@ struct Mahasiswa {
 	DetailAlamat alamat;
 	int umur;
 };
+int main() {
+	Mahasiswa mhs[3];
+	for (int i = 0; i < 3; i++) {
+		cout << "Data Mahasiswa Ke- " << (i + 1) << ":" << endl;
+		cout << "Nomor Mahasiswa = ";
+		getline(cin, mhs[i].nim);
+		cout << "Nama Mahasiswa = ";
+		getline(cin, mhs[i].nama);
+		cout << "Alamat Mahasiswa = " << endl;
+		cout << "\t nama Desa = ";
+		cin >> mhs[i].alamat.desa;
+		cout << "\t nama Kota = ";
+		cin >> mhs[i].alamat.kota;
+		cout << "Umur Mahsiswa = ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << endl;
+	}
+	for (int i = 0; i < 3; i++) {
+		cout << endl;
+		cout << "Data Mahasiswa Ke- " << (i + 1) << ":" << endl;
+		cout << "\n NIM = " << mhs[i].nim;
+		cout << "\n Nama = " << mhs[i].nama;
+		cout << "\n Alamat = ";
+		cout << "\n Desa = " << mhs[i].alamat.desa;
+		cout << "\n Kota = " << mhs[i].alamat.kota;
+		cout << "\n Umur = " << mhs[i].umur;
+		cout << endl;
+	}
+}
