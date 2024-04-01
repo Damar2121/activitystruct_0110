370b9ebe1d758af03b84d3457a52d540dd330da8
Damar2121 <damar.vivoy51@gmail.com>
2024-04-01 16:23:46 +0700

    membuat hasil data mahasiswa dan memperbaiki error line 12

--- StructC_1/StructC_1.cpp
+++ StructC_1/StructC_1.cpp
@@ -9,7 +9,7 @@ struct mahasiswa {
 };
 
 int main() {
-	Mahasiswa mhs;
+	mahasiswa mhs;
 	cout << "Nomoer Mahasiswa = ";
 	cin >> mhs.nim;
 	cout << "Nama Mahasiswa = ";
@@ -18,4 +18,10 @@ int main() {
 	cin >> mhs.alamat;
 	cout << "Umur Mahsiswa = ";
 	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n NIM = " << mhs.nim;
+	cout << "\n Nama = " << mhs.nama;
+	cout << "\n Alamat = " << mhs.alamat;
+	cout << "\n Umur = " << mhs.umur;
 }
